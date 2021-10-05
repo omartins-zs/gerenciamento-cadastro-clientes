7bc61f931e9d6a3adfc2e9f97840ae0133baf62d
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-05 17:46:04 -0300

    Criando Table De Clientes

--- index.php
+++ index.php
@@ -15,6 +15,46 @@
                 <i class="fa fa-user-plus"></i>
             </a>
         </h5>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead class="thead">
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>CPF</th>
+                        <th>Dt Nasc</th>
+                        <th>Endereço</th>
+                        <th>Telefone</th>
+                        <th colspan="3">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nome</td>
+                        <td>Email</td>
+                        <td>CPF</td>
+                        <td>Dt Nasc</td>
+                        <td>Enderelo</td>
+                        <td>Telefone</td>
+                        <td>
+                            <form action="" method="POST">
+                                <button class="btn btn-warning btn-xs">
+                                    <i class="fa fa-user-edit"></i>
+                                </button>
+                            </form>
+                        </td>
+                        <td>
+                            <form action="" method="POST">
+                                <button class="btn btn-danger btn-xs">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                        </td>
+
+                    </tr>
+                </tbody>
+
+        </div>
     </div>
 </body>
 
