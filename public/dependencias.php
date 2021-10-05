155ee7dd243d6ade68d84b0c18ec5e5f9a683002
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-05 17:31:36 -0300

    Adicionando dependencias

---
+++ public/dependencias.php
@@ -0,0 +1,51 @@
+<meta charset="UTF-8">
+
+<title>Cadastro Clientes</title>
+
+
+<!-- video bootstrap -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+    integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+
+<!-- video font awesome -->
+
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+
+<style>
+body {
+    margin: 0;
+    background-color: #fff;
+}
+
+
+* {
+    font-family: 'Open Sans', sans-serif;
+}
+
+
+h2 {
+    font-family: 'Open Sans', sans-serif;
+}
+
+thead {
+    background-color: #f7f7f7;
+}
+</style>
+
+
+<!-- 
+
+Projeto Anterior sem final 
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+   
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+-->
