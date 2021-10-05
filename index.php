9ae9afc83f789e0587d818479a30b0202b60654a
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-05 17:32:09 -0300

    Criando Index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once 'public/dependencias.php'?>
+</head>
+
+<body>
+    <div class="container">
+        <h2 class="text-center">
+            Lista de Clientes <i class="fa fa-users"></i>
+        </h2>
+        <h5 class="text-right">
+            <a href="views/page_register.php" class="btn btn-primary btn-xs">
+                <i class="fa fa-user-plus"></i>
+            </a>
+        </h5>
+    </div>
+</body>
+
+</html>
