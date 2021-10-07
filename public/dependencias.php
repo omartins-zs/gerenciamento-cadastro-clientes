a10defac6512d7a5da889655f8b7edf17c5e7a4a
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-07 19:05:02 -0300

    Reajuste Style

--- public/dependencias.php
+++ public/dependencias.php
@@ -2,16 +2,13 @@
 
 <title>Cadastro Clientes</title>
 
+<!-- Bootstrap 4 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-<!-- video bootstrap -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-    integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
-
-<!-- video font awesome -->
-
-<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+<!-- Fontawesome 5 -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
+    integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
 
 <style>
@@ -32,6 +29,19 @@ h2 {
 
 thead {
     background-color: #f7f7f7;
+}
+
+.text-center {
+    margin: 2rem 0 1rem 0;
+    text-align: center;
+}
+
+.text-right {
+    text-align: right;
+}
+
+.table-responsive {
+    border-radius: 20px;
 }
 </style>
 
